9b28d7771b3ff2930244d0b8fe7be3881c1d127d
Andrew Ingalls <aingalls@stowers.org>
2022-08-12 10:53:43 -0500

    test git ignore

---
+++ config.py
@@ -0,0 +1,5 @@
+username = "***REMOVED***"
+password = "***REMOVED***"
+host = "***REMOVED***"
+dbase = "***REMOVED***"
+dir_name = "L:/***REMOVED***/Facility/Life Support Systems/PLC Data Logs/Individual Rack Data"
