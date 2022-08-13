b6218cef8b63509029a57a9da2731a3fa375cf1e
Cavefish1 <cavefish1@stowers.org>
2022-08-13 10:59:46 -0500

    functions worked!

--- code/DAG_functions.py
+++ code/DAG_functions.py
@@ -208,7 +208,7 @@ def postgres_upsert(table, conn, keys, data_iter):
     conn.execute(upsert_statement)
 
 
-def loadData(alarm_df):
+def loadData(device_df, sensor_df, alarm_df):
     """
     ingests dataframes that were transformed into the postgres database
 
@@ -224,18 +224,18 @@ def loadData(alarm_df):
     conn = db.connect()
 
 
-    # device_df.to_sql('device_log',
-    #                  conn,
-    #           if_exists='append',
-    #           index=False,
-    #           method=postgres_upsert)
+    device_df.to_sql('device_log',
+                     conn,
+              if_exists='append',
+              index=False,
+              method=postgres_upsert)
 
 
-    # sensor_df.to_sql('sensor_log',
-    #                  conn,
-    #                  if_exists='append',
-    #                  index=False,
-    #                  method=postgres_upsert)
+    sensor_df.to_sql('sensor_log',
+                     conn,
+                     if_exists='append',
+                     index=False,
+                     method=postgres_upsert)
 
     alarm_df.to_sql('alarm_log',
                     conn,
--- code/test_dag.py
+++ code/test_dag.py
@@ -9,4 +9,4 @@ device_df, sensor_df, alarm_df = transformData(device_df, sensor_df, alarm_df)
 
 loadData(device_df, sensor_df, alarm_df)
 
-#deleteFiles(dir_name)
+deleteFiles(dir_name)
